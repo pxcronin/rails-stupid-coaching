f72d9c9c7a0aecb4486864f271ba82c39963e2c7
Patrick Cronin <pxcronin@gmail.com>
2020-11-09 11:51:22 -0500

    Everything is up and running

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,17 @@
 class QuestionsController < ApplicationController
   def ask
+    @question = params[:question]
   end
 
   def answer
+    @question = params[:question]
+    case
+    when @question == 'I am going to work'
+      @answer = 'Great!'
+    when @question.last == '?'
+      @answer = 'Silly question, get dressed and go to work!'      
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
